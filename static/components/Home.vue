d7d7033e9b20f68892838898aa30e8bb28b56ea5
adigulalkari <adigulalkari825@gmail.com>
2024-02-15 16:27:01 +0530

    Started to convert js to vue

---
+++ static/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div> 
+        <AdminHome v-if="userRole === 'admin'" />
+        <CustomerHome v-else-if="userRole === 'customer'" />
+        <ManagerHome v-else-if="userRole === 'manager'" />
+        <Landing v-else />
+    </div>
+</template>
+
+<script>
+import AdminHome from "./AdminHome.js"
+import CustomerHome from "./CustomerHome.js"
+import ManagerHome from "./ManagerHome.js"
+import Landing from "./Landing.js"
+
+export default {
+    data() {
+        return {
+            userRole: localStorage.getItem('role')
+        }
+    },
+    components: {
+        AdminHome,
+        CustomerHome,
+        ManagerHome,
+        Landing
+    }
+}
+</script>
